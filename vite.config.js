b3de10a6dd46a6cf3e0d7e9e626f5d4feecb4f91
lily416 <daconcha@uc.cl>
2026-01-12 16:23:03 -0300

    fix: build link

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/2024_s2',
   cors: true,
   server: {
     watch: {
